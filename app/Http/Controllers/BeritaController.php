d75537c56661c386217eaae7f0af9a4cfd2de352
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2024-07-24 04:12:15 +0800

    berita terbaru sisa dibuatkan link untuk lihat detail

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
 
 class BeritaController extends Controller
@@ -21,9 +22,14 @@ class BeritaController extends Controller
             usort($data, function($a, $b) {
                 return strtotime($b['tanggal_publikasi']) - strtotime($a['tanggal_publikasi']);
             });
-
+            
             // Ambil berita paling terbaru
             $latestNews = $data[0];
+            
+            if ($latestNews) {
+                $latestNews['summary'] = Str::limit($latestNews['isi_berita'], 450);
+            }
+
 
             return view('landing.index', ['data' => $latestNews]);
         } catch (\Exception $e) {
--- resources/views/partials/berita_terbaru.blade.php
+++ resources/views/partials/berita_terbaru.blade.php
@@ -7,7 +7,9 @@
     <div class="col-lg-6 no-padding home-about-right">
             <h1>{{ $data['judul'] }}</h1>
             <p><span>{{ $data['penulis'] }}<br>{{ $data['tanggal_publikasi'] }}</span></p>
-            <p>{{ $data['isi_berita'] }}</p>
+            <div class="">
+                {!! $data['summary'] !!}
+            </div>
         <a class="text-uppercase primary-btn" href="{{ url('/berita-terbaru') }}">Selengkapnya</a>
     </div>
     </div>
